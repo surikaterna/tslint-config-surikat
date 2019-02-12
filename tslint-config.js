ef52a5372226918ccd836908c54733df52e51e2d
patriknygren <patriknygren82@gmail.com>
2019-02-12 15:15:12 +0100

    rules matching eslint

--- tslint-config.js
+++ tslint-config.js
@@ -1,3 +1,11 @@
 module.exports = {
-    "extends": "tslint-config-airbnb"
+    "defaultSeverity": "error",
+    "extends": "tslint-config-airbnb",
+    "rules": {
+        "trailing-comma": [true, {"multiline": "never", "singleline": "never"}],
+        "no-irregular-whitespace": true,
+        "max-line-length": [true, 160]
+    },
+    "jsRules": true,
+    "rulesDirectory": []
 }
